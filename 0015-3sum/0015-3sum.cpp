a16614a18fa8ce0f6453a18604ef8767c1cf3e4e
Prashant Pandey <prashantpandey3103@gmail.com>
2026-04-10 12:49:40 +0530

    Fix condition to skip duplicate right elements

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -17,7 +17,7 @@ public:
                     left++;
                     right--;
                     while(left < right && nums[left] == nums[left-1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                 }else if(sum < 0){
                     left++;
                 }else{
